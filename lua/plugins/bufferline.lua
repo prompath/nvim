3d31682adcc1bd7df7fec6e521c29eab616bfda2
Jeerawat Chuachan <jeerawatchu@gmail.com>
2024-05-15 03:58:15 +0700

    Add bufferline offset for file explorer

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,6 +3,18 @@ return {
   lazy = false,
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
-    require("bufferline").setup()
+    require("bufferline").setup({
+      options = {
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            text_align = "center",
+            separator = true
+          }
+        }
+      }
+    }
+    )
   end
 }
